93f4340e0677769f4dedec818559eb15224b0150
fransonsr <fransonsr@familysearch.org>
2017-11-30 15:32:45 -0700

    Add @EnableRetry to gateway config.

--- cli/gateway/src/main/java/com/example/videocat/cli/VideoCatAPIGatewayConfiguration.java
+++ cli/gateway/src/main/java/com/example/videocat/cli/VideoCatAPIGatewayConfiguration.java
@@ -2,18 +2,20 @@ package com.example.videocat.cli;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableRetry
 public class VideoCatAPIGatewayConfiguration {
 
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
-	@Bean
-	public VideoCatAPIGateway gateway(RestTemplate restTemplate) {
-		return new VideoCatAPIGateway(restTemplate);
-	}
+    @Bean
+    public VideoCatAPIGateway gateway(RestTemplate restTemplate) {
+        return new VideoCatAPIGateway(restTemplate);
+    }
 }
